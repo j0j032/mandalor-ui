23c5710c4f7cbb2ce3f3d60eaa4873e8f561d1fe
Jordan Martinellli <hello@j0j0.me>
2025-03-03 17:11:45 +0100

    add matching pairs

---
+++ packages/palettes/src/matching-pairs.ts
@@ -0,0 +1,8 @@
+export const gray = ['all']
+export const mauve = ['tomato', 'red', 'ruby', 'crimson', 'pink', 'plum', 'purple', 'violet']
+export const slate = ['iris', 'indigo', 'blue', 'sky', 'cyan']
+export const sage = ['mint', 'teal', 'jade', 'green']
+export const olive = ['grass', 'lime']
+export const sand = ['amber', 'bronze', 'amber', 'gold', 'yellow', 'brown']
+
+export const hasDarkTextOnSolidBackground = ['lime', 'sky', 'yellow', 'amber', 'mint']
